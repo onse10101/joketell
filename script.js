4b0481b79bfd63942ca01675d3a8968f5ffb9704
Sydney Charles <68644060+onse10101@users.noreply.github.com>
2023-04-26 01:18:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+const button = document.getElementById('button');
+const audioElement = document.getElementById('audio'); 
+
+// Disable/Enable Button
+function toggleButton() {
+  button.disabled = !button.disabled;
+}
+
+// Passing Joke to VoiceRSS API
+function tellMe(joke) {
+    VoiceRSS.speech({
+    key: 'a567e5731ca44059a5e935a77e2abd70',
+    src: joke,
+    hl: 'en-us',
+    v: 'Linda',
+    r: 0, 
+    c: 'mp3',
+    f: '44khz_16bit_stereo',
+    ssml: false
+  });
+}
+
+// Get Jokes from Joke API
+async function getJokes() {
+  let joke = '';
+  const apiUrl = 'https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist';
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    if (data.setup) {
+      joke = `${data.setup} ... ${data.delivery}`;
+    }else {
+      joke = data.joke;
+    }
+    // Text to speech
+    tellMe(joke);
+    // Disable button
+    toggleButton();
+  } catch (error) {
+    // Catch error
+    console.log('hiiii', error);
+  }
+}
+
+// Event Listeners
+button.addEventListener('click', getJokes);
+audioElement.addEventListener('ended', toggleButton);
